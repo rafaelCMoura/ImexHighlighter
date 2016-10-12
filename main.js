c6f4312681144c3c91a8f6d499eaaf4e999d4847
Rafael Cabral de Moura <rafael.xuxubelez@gmail.com>
2016-10-11 23:58:19 -0300

    adds some regex

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
 define(function (require, exports, module) {
 	'use strict';
 
-
+    brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
+    
     // Defining a new CodeMirror mode to use with brackets:
-    CodeMirror.defineMode("imex", function (config, parserConfig) {
+    CodeMirror.defineMode("imex", function (config, parseConfig){
         
+        var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+        ExtensionUtils.loadStyleSheet(module, "styles/styles.css");
+        
+        return {
+            token: function (stream, state) {
+            
+                // String
+                if (stream.match(/(\"[\w\s]+\")/) {
+                    stream.skipToEnd();
+                    return 'imex_string';
+                }
+        
+                // Number
+                if (stream.match(/\d+((.|,)\d+)?/)) {
+                    return 'imex_number';
+                }
+                
+                // Keyword
+                if (stream.match(/(por|POR|\*POR)/)) {
+                    return 'imex_card';
+                }
+        
+                // Skip everything else
+                stream.eat(/./);
+            },
+            startState: function(){ return {inComment: false}; }
+        };
     });
     
-    CodeMirror.defineMIME("text/imex", {
-        
-    });
-
     var LanguageManager = brackets.getModule("language/LanguageManager");
     
     LanguageManager.defineLanguage("imex", {
-        name: "IMEX",
-        mode: ["IMEX", "text/imex"],
+        name: "imex",
+        mode: ["imex", "text/imex"],
         fileExtensions: [".dat"],
-        blockComment: ["*", "*"], // Verificar comentario
-        lineComment: ["**"]       // Verificar
+        blockComment: ["**", "**"],
+        lineComment: ["**"]
     });
     
     
