b273091a10ffe50174a2da59756e5d705c842ef4
Rafael Moura <rafael.xuxubelez@gmail.com>
2016-10-13 15:43:20 -0300

    Fixes brackets getmodule warning

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 define(function (require, exports, module) {
 	'use strict';
 
-    brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
+    brackets.getModule("thirdparty/CodeMirror/lib/codemirror");
     
     // Defining a new CodeMirror mode to use with brackets:
     CodeMirror.defineMode("imex", function (config, parseConfig) {
